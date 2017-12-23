f1864d2caa010baf21e42301ca34f58f65aad8f6
Ian Dees <ian.dees@gmail.com>
2017-12-23 12:54:15 -0600

    Add 2016 5yr config

--- meta-scripts/build_sql_files.py
+++ meta-scripts/build_sql_files.py
@@ -145,6 +145,11 @@ config = {
         'line_number_column_name': 'Line Number',
         'table_id_column_name': 'Table ID'
     },
+    'acs2016_5yr': {
+        'sequence_number_column_name': 'Sequence Number',
+        'line_number_column_name': 'Line Number',
+        'table_id_column_name': 'Table ID'
+    },
 }
 
 if which_acs not in config:
